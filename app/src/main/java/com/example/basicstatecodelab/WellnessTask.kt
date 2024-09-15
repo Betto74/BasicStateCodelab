d8577d5e7f14b3c2d0862337304667ba1a556151
Betto74 <s21120191@alumnos.itsur.edu.mx>
2024-09-15 12:44:31 -0600

    12. Estado en ViewModel

--- app/src/main/java/com/example/basicstatecodelab/WellnessTask.kt
+++ app/src/main/java/com/example/basicstatecodelab/WellnessTask.kt
@@ -1,3 +1,14 @@
 package com.example.basicstatecodelab
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 
-data class WellnessTask(val id: Int, val label: String)
+class WellnessTask(
+    val id: Int,
+    val label: String,
+    initialChecked: Boolean = false
+) {
+    var checked by mutableStateOf(initialChecked)
+}
